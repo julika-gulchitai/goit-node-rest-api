d096a4c4179deb528bb2042b038b7777fb19712d
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-02-24 22:53:45 +0200

    new branch and schema

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, setUpdateSetting } from "./hooks.js";
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+export const User = model("user", userSchema);
